445edfbdec7818f9bd887e7752abe78e06d96a91
Benixonthomas <Benixonthomas>
2024-03-14 02:00:30 -0400

    committing files

---
+++ providers.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  # skip_provider_registration = true
+
+  features {}
+  subscription_id = "f68c3af1-22a7-4a8b-9d5d-44b5a4eb6ebc"
+  tenant_id = "73c4eab0-8e5e-476e-af6f-3d893a8ad11d"
+}
